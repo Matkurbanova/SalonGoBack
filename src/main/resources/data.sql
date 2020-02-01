988ec18d49b4c11b6fe3ab7a06ae9949e0cce184
Bermet <bermetmatkurbanova@gmail.com>
2020-02-02 00:10:28 +0600

    UserSalon add typeStatus

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -104,14 +104,14 @@ VALUES
 
 
 
-INSERT INTO UserSalon(id,login,name,password,phone,address,description,instaLogin,logoSalon)
+INSERT INTO UserSalon(id,login,name,password,phone,address,description,instaLogin,logoSalon,typeStatus)
 VALUES(1,'bermet03','Bermet','123','+996770595959','5-й микрорайон, 63Б
 5 м-н, Октябрьский район, Бишкек','Мы всегда рады видеть новых гостей в нашем салоне красоты!
 Широкий спектр услуг, отличное качество - по приемлемым ценам.','salon03','imageLogo.png',1);
-INSERT INTO UserSalon(id,login,name,password,phone,address,description,instaLogin,logoSalon)
+INSERT INTO UserSalon(id,login,name,password,phone,address,description,instaLogin,logoSalon,typeStatus)
 VALUES(2,'nurzhan05','Nurzhan','1234','+996770595985','5-й микрорайон, 63Б
 5 м-н, Октябрьский район, Бишкек','Мы всегда рады видеть новых гостей в нашем салоне красоты!
-Широкий спектр услуг, отличное качество - по приемлемым ценам.','arapova.nn','imageLogo.png');
+Широкий спектр услуг, отличное качество - по приемлемым ценам.','arapova.nn','imageLogo.png',1);
 
 
 
